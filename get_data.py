dd6eda61eedd14c16ffbf2dc6b94297c299f5b12
Dave Bunyan <davebunyan1@gmail.com>
2026-09-21 16:17:37 -0400

    Pure aiosqlite benchmark

--- get_data.py
+++ get_data.py
@@ -1,14 +1,16 @@
 import asyncio
 import time
 
+import aiosqlite
 from sqlalchemy import text
 
 from database import async_session_maker
 
+DB_PATH = "./mydb.db"
+
 
 async def get_transaction_for_user(user_id: int):
-    sql = text(
-        """
+    sql = """
         SELECT 
             ticker,
             SUM(shares) AS shares,
@@ -18,17 +20,14 @@ async def get_transaction_for_user(user_id: int):
                 ELSE SUM(shares * price_per_share) / SUM(shares) 
             END AS price_per_share
         FROM transactions
-        WHERE user_id = :user_id
+        WHERE user_id = ?
         GROUP BY ticker;
     """
-    )
 
-    async with async_session_maker() as session:
-        result = await session.execute(sql, {"user_id": user_id})
-        rows = result.all()
-
-        for row in rows:
-            print(f"{row.ticker}: {row.shares} {row.cost_basis} {row.price_per_share}")
+    async with aiosqlite.connect(DB_PATH) as db:
+        async with db.execute(sql, (user_id,)) as cursor:
+            rows = await cursor.fetchall()
+            return rows
 
 
 if __name__ == "__main__":
